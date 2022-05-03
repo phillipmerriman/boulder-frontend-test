5bebf3e20e4ba920dc724e5b9b076a97c7de7517
Phillip Merriman <phillipmerriman@gmail.com>
2022-05-02 21:04:06 -0500

    data rendered to graph

--- src/App.js
+++ src/App.js
@@ -1,22 +1,85 @@
-import logo from './logo.svg';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+
 import './App.css';
+import data from './workout-data.json';
 
 function App() {
+
+  // const mockData = [
+  //   {
+  //     name: 'Page A',
+  //     uv: 4000,
+  //     power: 2400,
+  //     amt: 2400,
+  //   },
+  //   {
+  //     name: 'Page B',
+  //     uv: 3000,
+  //     power: 1398,
+  //     amt: 2210,
+  //   },
+  //   {
+  //     name: 'Page C',
+  //     uv: 2000,
+  //     power: 9800,
+  //     amt: 2290,
+  //   },
+  //   {
+  //     name: 'Page D',
+  //     uv: 2780,
+  //     power: 3908,
+  //     amt: 2000,
+  //   },
+  //   {
+  //     name: 'Page E',
+  //     uv: 1890,
+  //     power: 4800,
+  //     amt: 2181,
+  //   },
+  //   {
+  //     name: 'Page F',
+  //     uv: 2390,
+  //     power: 3800,
+  //     amt: 2500,
+  //   },
+  //   {
+  //     name: 'Page G',
+  //     uv: 3490,
+  //     power: 4300,
+  //     amt: 2100,
+  //   },
+  // ];
+
+  const realData = data.samples.map(item => {
+    return {
+      millisecondOffset: item.millisecondOffset,
+      power: item.values.power
+    }
+  });
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ResponsiveContainer width="100%" aspect={3}>
+          <LineChart
+            width={500}
+            height={300}
+            data={realData}
+            margin={{
+              top: 5,
+              right: 30,
+              left: 20,
+              bottom: 5,
+            }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="millisecondOffset" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Line type="monotone" dataKey="power" stroke="#8884d8" activeDot={{ r: 8 }} />
+          </LineChart>
+        </ResponsiveContainer>
       </header>
     </div>
   );
