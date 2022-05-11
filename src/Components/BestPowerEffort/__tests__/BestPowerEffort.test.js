689e8fb64f6b19f1fa4548b5471cf6dd7d32f187
Phillip Merriman <phillipmerriman@gmail.com>
2022-05-11 09:21:28 -0500

    updated tests for BestPowerEffort and GraphTip

--- src/Components/BestPowerEffort/__tests__/BestPowerEffort.test.js
+++ src/Components/BestPowerEffort/__tests__/BestPowerEffort.test.js
@@ -4,24 +4,28 @@ import BestPowerEffort from '../BestPowerEffort';
 import { render, cleanup, screen } from '@testing-library/react';
 import renderer from 'react-test-renderer';
 
-afterEach(cleanup);
+describe('BestPowerEffort component', () => {
 
-it('renders without crashing', () => {
-    const div = document.createElement("div");
-    ReactDOM.render(<BestPowerEffort></BestPowerEffort>, div);
-});
-
-it('renders best 1 minute power effort correctly', () => {
-    render(<BestPowerEffort oneMinute={300}></BestPowerEffort>);
-    expect(screen.getByTestId('bestOneMinute')).toHaveTextContent(300);
-});
-
-it('renders best 20 minute power effort correctly', () => {
-    render(<BestPowerEffort twentyMinute={200}></BestPowerEffort>);
-    expect(screen.getByTestId('bestTwentyMinute')).toHaveTextContent(200);
-});
-
-it('matches snapshot', () => {
-    const tree = renderer.create(<BestPowerEffort twentyMinute={200} oneMinute={300}></BestPowerEffort>).toJSON();
-    expect(tree).toMatchSnapshot();
+    afterEach(cleanup);
+    
+    it('renders without crashing', () => {
+        const div = document.createElement("div");
+        ReactDOM.render(<BestPowerEffort></BestPowerEffort>, div);
+    });
+    
+    it('renders best 1 minute power effort correctly', () => {
+        render(<BestPowerEffort oneMinute={300}></BestPowerEffort>);
+        expect(screen.getByTestId('bestOneMinute')).toHaveTextContent(300);
+    });
+    
+    it('renders best 20 minute power effort correctly', () => {
+        render(<BestPowerEffort twentyMinute={200}></BestPowerEffort>);
+        expect(screen.getByTestId('bestTwentyMinute')).toHaveTextContent(200);
+    });
+    
+    it('matches snapshot', () => {
+        const tree = renderer.create(<BestPowerEffort twentyMinute={200} oneMinute={300}></BestPowerEffort>).toJSON();
+        expect(tree).toMatchSnapshot();
+    });
+    
 });
--- src/Components/GraphTip/__tests__/GraphTip.test.js
+++ src/Components/GraphTip/__tests__/GraphTip.test.js
@@ -4,19 +4,23 @@ import GraphTip from '../GraphTip';
 import { render, cleanup, screen } from '@testing-library/react';
 import renderer from 'react-test-renderer';
 
-afterEach(cleanup);
+describe('GraphTip component', () => {
 
-it('renders without crashing', () => {
-    const div = document.createElement("div");
-    ReactDOM.render(<GraphTip />, div);
-});
-
-it('renders axis info correctly', () => {
-    render(<GraphTip />);
-    expect(screen.getByTestId('axisInfo')).toHaveTextContent('X-Axis: Milliseconds | Y-Axis: Power Output');
-});
-
-it('matches snapshot', () => {
-    const tree = renderer.create(<GraphTip />).toJSON();
-    expect(tree).toMatchSnapshot();
+    afterEach(cleanup);
+    
+    it('renders without crashing', () => {
+        const div = document.createElement("div");
+        ReactDOM.render(<GraphTip />, div);
+    });
+    
+    it('renders axis info correctly', () => {
+        render(<GraphTip />);
+        expect(screen.getByTestId('axisInfo')).toHaveTextContent('X-Axis: Milliseconds | Y-Axis: Power Output');
+    });
+    
+    it('matches snapshot', () => {
+        const tree = renderer.create(<GraphTip />).toJSON();
+        expect(tree).toMatchSnapshot();
+    });
+    
 });
